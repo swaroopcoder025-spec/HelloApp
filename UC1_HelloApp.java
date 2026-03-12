a5217409b18ca31a2714cd142480c30c8c56d487
swaroop <swaroopgamer025@gmail.com>
2026-03-13 01:20:40 +0530

    Removed unnecessary file

--- UC1_HelloApp.java
+++
@@ -1,5 +0,0 @@
-public class UC1_HelloApp {
-    public static void main(String[] args) {
-        System.out.println("Hello App");
-    }
-}
--- UC2_HelloApp.java
+++
@@ -1,6 +0,0 @@
-public class UC2_HelloApp {
-    public static void main(String[] args) {
-        String name = args[0];
-        System.out.println("Hello " + name);
-    }
-}
