d67612fced88f4382b638ef53da73c3b78b2be44
swaroop <swaroopgamer025@gmail.com>
2026-03-17 13:49:08 +0530

    UC6

---
+++ UC6_HelloApp.java
@@ -0,0 +1,25 @@
+public class UC6_HelloApp{
+
+    public static void main(String[] args){
+
+     StringBuilder nameBuilder = new StringBuilder();
+
+     for (String name : args){
+
+        nameBuilder.append(name).append(", ");
+
+     }
+
+          String name = "";
+
+         if (nameBuilder.length() > 0) {
+
+             name = nameBuilder.substring(0 , nameBuilder.length() - 2);
+
+         }
+
+         System.out.println("Hello, " + name + "!");
+
+    }
+
+}
